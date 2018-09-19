5eb34907918fd091907425ae643a1c83e4924eac
Eric Fiorani <efiorani305@gmail.com>
2018-09-19 12:01:33 +0000

    added to app controllers, and welcome page (html & css)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,4 +18,23 @@ class ApplicationController < Sinatra::Base
     erb :'/index'
   end
 
+  get "/logout" do
+    if logged_in?
+      session.clear
+      redirect "/login"
+    else
+      redirect "/"
+    end
+  end
+
+  helpers do
+    # def logged_in?
+    #   !!current_user
+    # end
+    #
+    # def current_user
+    #   User.find_by(id: session[user_id])
+    # end
+  end
+
 end
---
+++ app/controllers/wod_controller.rb
@@ -0,0 +1,2 @@
+class WodController < ApplicationController
+end
