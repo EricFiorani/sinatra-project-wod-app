9a5c3997e3cd636b71d7ed3e48bd7fe7796b06f6
Eric Fiorani <efiorani305@gmail.com>
2018-10-07 18:10:51 +0000

    more comments added for study guide

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 require './config/environment'
 require 'sinatra/base'
+#Never require 'sinatra' in your extension. You should only ever need to require
+# 'sinatra/base'. The reason for this is that require 'sinatra' is what triggers
+# the classic style – extensions should never trigger the classic style.
 require 'rack-flash'
 
 
@@ -23,13 +26,16 @@ class ApplicationController < Sinatra::Base
     redirect '/'
   end
 
-  helpers do
+  helpers do #adds methods for use in routes, views, and helper methods.
     def logged_in?
       !!current_user
     end
 
     def current_user
       User.find_by(id: session[:user_id])
+      #Finds the first record matching the specified conditions.
+      # There is no implied ordering so if order matters, you should specify 
+      # it yourself. If no record is found, returns nil.
     end
   end
 
