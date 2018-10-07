58d99d855d408fa87c02ef60e26e7dace1b5eb51
Eric Fiorani <efiorani305@gmail.com>
2018-10-07 20:33:29 +0000

    more comments added for study prep

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,8 @@ require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 ActiveRecord::Base.establish_connection(
+#using the establish_connection method from ActiveRecord::Base to connect
+#to our users database, which will be created in the migration via SQLite3 (the adapter).
   :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
